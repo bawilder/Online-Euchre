d7e6bd581609a859a8134caf378fef9e8f33f2dc
Honorbound156 <Hmhattis@mtu.edu>
2016-02-26 12:52:08 -0500

    Updated Euchre Game.java
    This update makes the current game a bit more playable, fixes a bug with the card playing mechanism, adds more friendly print statements to catch card playing exceptions, adds print statements to tell score and tricks landed more clearly.

--- src/EuchreGame.java
+++ src/EuchreGame.java
@@ -37,35 +37,67 @@ public class EuchreGame {
 		//use player turn, use dealer, use read ins, use play card
 		
 		//play all the cards for the trick
-		System.out.println("Player 1, play a card from this hand: (0-4 are the viable inputs)");
+		System.out.println("Player 1, play a card from this hand: (1-5 are the viable inputs)");
 		table.players[0].showHand();
-		int card1 = in.nextInt();
+		int card1 = in.nextInt()-1 ;
+		
+		while(card1< 0 || card1 > 4 || table.players[0].hand[card1] == null){
+			System.out.println("Invalid card, try again... (That card isn't in your hand)");
+			card1= in.nextInt()-1;
+		}
+		
 		table.tableCards[0]= table.players[0].playCard(card1);
 		
 		
-		System.out.println("Player 2, play a card from this hand: (0-4 are the viable inputs)");
+		System.out.println("Player 2, play a card from this hand: (1-5 are the viable inputs)");
 		table.players[1].showHand();
-		int card2 = in.nextInt();
+		int card2 = in.nextInt()-1;
+		
+		while(card2 < 0 || card2 > 4 || table.players[1].hand[card2] == null){
+			System.out.println("Invalid card, try again... (That card isn't in your hand)");
+			card3= in.nextInt()-1;
+		}
+		
 		table.tableCards[1]= table.players[1].playCard(card2);
 		
-		System.out.println("Player 3, play a card from this hand: (0-4 are the viable inputs)");
+		System.out.println("Player 3, play a card from this hand: (1-5 are the viable inputs)");
 		table.players[2].showHand();
-		int card3 = in.nextInt();
+		int card3 = in.nextInt()-1;
+		
+		while(card3 < 0 || card3 > 4 || table.players[2].hand[card3] == null){
+			System.out.println("Invalid card, try again... (That card isn't in your hand)");
+			card3= in.nextInt()-1;
+		}
+		
 		table.tableCards[2]= table.players[2].playCard(card3);
 		
-		System.out.println("Player 4, play a card from this hand: (0-4 are the viable inputs)");
+		System.out.println("Player 4, play a card from this hand: (1-5 are the viable inputs)");
 		table.players[3].showHand();
-		int card4 = in.nextInt();
+		int card4 = in.nextInt()-1;
+		
+		while(card4 < 0 || card4 > 4 || table.players[3].hand[card4] == null){
+			System.out.println("Invalid card, try again... (That card isn't in your hand)");
+			card4= in.nextInt()-1;
+		}
+		
 		table.tableCards[3]= table.players[3].playCard(card4);
 		
 		//evaluate cards played
-		Card maxCard= new Card('A',0,'J');
+		Card maxCard= new Card('A',0,'A');
 		int playerOfCard=0;
-		for(int i=0; i<4; i+=1){
-			if(maxCard.value < table.tableCards[i].value){
+		for(int i=0; i<3; i+=1){
+			
+			//if statement until card evaluation is more accurate
+			if(maxCard.value <= table.tableCards[i].value){
 				maxCard=table.tableCards[i];
 				playerOfCard=i;
 			}
+			
+			//this will be the actual if after evaluation is cleaned up
+			//if(maxCard.value < table.tableCards[i].value){
+			//	maxCard=table.tableCards[i];
+			//	playerOfCard=i;
+			//}
 		}
 		
 		System.out.println("Player " + (playerOfCard+1) +  " wins the trick!");
@@ -91,10 +123,19 @@ public class EuchreGame {
 		if(table.team1.getTricks()>2){
 			table.team1.teamScored(1);
 			System.out.println("Team 1 scored!");
+			System.out.println("Player 1 earned " + table.players[0].tricks + " trick(s), and Player 3 earned " + table.players[2].tricks + " trick(s).");
+			table.players[0].clearTricks();
+			table.players[2].clearTricks();
+			System.out.println();
 		}
+		
 		else{
 			table.team2.teamScored(1);
 			System.out.println("Team 2 scored!");
+			System.out.println("Player 2 earned " + table.players[1].tricks + " tricks, and Player 4 earned " + table.players[3].tricks + " tricks.");
+			table.players[1].clearTricks();
+			table.players[3].clearTricks();
+			System.out.println();
 		}
 		
 	}
