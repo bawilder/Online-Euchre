2e2ce6772ae01f3e871fc06b50374289ca518a5c
Honorbound156 <Hmhattis@mtu.edu>
2016-03-02 14:11:04 -0500

    Fix Euchre Game
    This commit fixes the off by one error introduced on 3/2/16

--- src/EuchreGame.java
+++ src/EuchreGame.java
@@ -7,14 +7,14 @@ import java.util.Scanner;
  * @class "EuchreGame" - Euchre headclass
  */
 public class EuchreGame {
-
+	
 	//class variables
 	//boolean to control the game state
 	Boolean gameOver= false;
 	//Table to initialize teams and players
 	Table table= new Table();
 	Scanner in = new Scanner(System.in);
-
+	
 	/*
 	 * This method will handle shuffling and dealing
 	 * the deck to start each hand
@@ -28,92 +28,88 @@ public class EuchreGame {
 		table.players[3].hand= table.deck.hand4;
 		table.topOfDiscard= table.deck.discard[0];
 	}
-
+	
 	public void trumpRound(){
-
+		
 	}
-
+	
 	public void runTrick(){
 		//use player turn, use dealer, use read ints, use play card
-
+		
 		//play all the cards for the trick
 		System.out.println("Player 1, play a card from this hand: (1-5 are the cards)");
 		table.players[0].showHand();
 		int card1 = in.nextInt()-1;
-
-
+		
+		
 		while( card1 < 0 || card1 > 4|| table.players[0].hand[card1] == null){
 			System.out.println("Invalid card, try again... (There arent that many cards in your hand)");
 			card1= in.nextInt()-1;
 		}
-
+		
 		table.tableCards[0]= table.players[0].playCard(card1);
-
-
+		
+		
 		System.out.println("Player 2, play a card from this hand: (1-5 are the cards)");
 		table.players[1].showHand();
 		int card2 = in.nextInt()-1;
-
+		
 		while( card2 < 0 || card2 > 4|| table.players[1].hand[card2] == null){
 			System.out.println("Invalid card, try again... (There arent that many cards in your hand)");
-
-			while(card2 < 0 || card2 > 4 || table.players[1].hand[card2] == null){
-				System.out.println("Invalid card, try again... (That card isn't in your hand)");
-				card2= in.nextInt()-1;
-			}
-
-			table.tableCards[1]= table.players[1].playCard(card2);
-
-			System.out.println("Player 3, play a card from this hand: (1-5 are the cards)");
-			table.players[2].showHand();
-			int card3 = in.nextInt()-1;
-
-			while( card3 < 0 || card3 > 4|| table.players[2].hand[card3] == null){
-				System.out.println("Invalid card, try again... (There arent that many cards in your hand)");
-				card3= in.nextInt()-1;
-			}
-
-			table.tableCards[2]= table.players[2].playCard(card3);
-
-			System.out.println("Player 4, play a card from this hand: (1-5 are the cards");
-			table.players[3].showHand();
-			int card4 = in.nextInt()-1;
-
-			while( card4 < 0 || card4 > 4|| table.players[3].hand[card4] == null){
-				System.out.println("Invalid card, try again... (There arent that many cards in your hand)");
-				card4= in.nextInt()-1;
-			}
-
-			table.tableCards[3]= table.players[3].playCard(card4);
-
-			//evaluate cards played
-			Card maxCard= new Card('9',0,'9');
-			int playerOfCard=0;
-
-			int maxCardPos= table.evalCards();
-			playerOfCard= maxCardPos;
-
-			System.out.println("Player " + (playerOfCard+1) +  " wins the trick!");
-
-			table.players[playerOfCard].tricks+=1;
-
-			if(playerOfCard==0 || playerOfCard==2){
-				table.team1.tricks+=1;
-			}
-			if(playerOfCard==1 || playerOfCard==3){
-				table.team2.tricks+=1;
-			}
+			card2= in.nextInt()-1;
 		}
+		
+		table.tableCards[1]= table.players[1].playCard(card2);
+		
+		System.out.println("Player 3, play a card from this hand: (1-5 are the cards)");
+		table.players[2].showHand();
+		int card3 = in.nextInt()-1;
+		
+		while( card3 < 0 || card3 > 4|| table.players[2].hand[card3] == null){
+			System.out.println("Invalid card, try again... (There arent that many cards in your hand)");
+			card3= in.nextInt()-1;
+		}
+		
+		table.tableCards[2]= table.players[2].playCard(card3);
+		
+		System.out.println("Player 4, play a card from this hand: (1-5 are the cards");
+		table.players[3].showHand();
+		int card4 = in.nextInt()-1;
+		
+		while( card4 < 0 || card4 > 4|| table.players[3].hand[card4] == null){
+			System.out.println("Invalid card, try again... (There arent that many cards in your hand)");
+			card4= in.nextInt()-1;
+		}
+		
+		table.tableCards[3]= table.players[3].playCard(card4);
+		
+		//evaluate cards played
+		Card maxCard= new Card('9',0,'9');
+		int playerOfCard=0;
 
+		int maxCardPos= table.evalCards();
+		playerOfCard= maxCardPos;
+		
+		System.out.println("Player " + (playerOfCard+1) +  " wins the trick!");
+		
+		table.players[playerOfCard].tricks+=1;
+		
+		if(playerOfCard==0 || playerOfCard==2){
+			table.team1.tricks+=1;
+		}
+		if(playerOfCard==1 || playerOfCard==3){
+			table.team2.tricks+=1;
+		}
+		
 	}
-
+	
 	public void runHand(){
 		dealDeck();
-
+		
 		for(int i=0; i<5; i+=1){
 			runTrick();
 		}
-
+		
 		if(table.team1.getTricks()>2){
 			table.team1.teamScored(1);
 			System.out.println("Team 1 scored!");
@@ -132,28 +128,28 @@ public class EuchreGame {
 			System.out.println("The Score of the game is: Team 1 has " + table.team1.score + " point(s) and Team 2 has " + table.team2.score + " point(s).");
 			System.out.println();
 		}
-
+		
 	}
-
+	
 	public void runGame(){
 		while(table.team1.getScore()<10 && table.team2.getScore()<10){
 			runHand();
 		}
-
+		
 		if(table.team1.winner == true){
 			System.out.println("Team 1 Won the Game");
 		}
-
+		
 		else{
 			System.out.println("Team 2 Won the Game");
 		}
 	}
-
-
-
+	
+	
+	
 	public static void main(String[] args) {
 		EuchreGame game= new EuchreGame();
 		game.runGame();
 	}
-
+	
 }//EOC
