82af08e414cb35c012093b11e19013a1ff24d6a6
tehowell <tannerhowell@rover-223-6.rovernet.mtu.edu>
2016-03-25 14:15:18 -0400

    Merge branch 'SHBrians' into Brian's
    # Conflicts:
    #	src/Packet.java

--- src/Packet.java
+++ src/Packet.java
@@ -205,19 +205,14 @@ public class Packet {
 	 */
 
 	//TODO: Pass the trump (face up card)
-	public String newHandPacket(int team1Score, int team2Score, int[] newHand, int discard, int dealerFlag) {
+
+	public String newHandPacket(int team1Score, int team2Score) {
 		String packet = "8,";
 		
 		//append team 1's score
 		packet = packet.concat(Integer.toString(team1Score) + ",");
 		//append team 2's score
 		packet = packet.concat(Integer.toString(team2Score) + ",");
-		//append the new hand
-		for(int i = 0; i < newHand.length; i++)
-			packet = packet.concat(Integer.toString(newHand[i]) + ",");
-		
-		//append the card that is currently face up
-		packet = packet.concat(Integer.toString(discard));
 		
 		//return the packet for sending
 		return packet;
